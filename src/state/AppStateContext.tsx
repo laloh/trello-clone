e8d99cf4aab020f9406f8d6d6555421d135ff28e
laloh <eduardocarhue@gmail.com>
2022-03-20 20:34:43 -0600

    Finish project

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -1,15 +1,21 @@
-import { createContext, useContext, FC, Dispatch } from "react";
+import { createContext, useContext, Dispatch, useEffect } from "react";
 import { appStateReducer, AppState, List, Task } from "./appStateReducer";
-import { useImmer, useImmerReducer } from "use-immer";
+import { useImmerReducer } from "use-immer";
 import { DragItem } from "../DragItem";
-
+import { save } from "../api";
 import { Action } from "./actions";
+import { withInitialState } from "../withInitialState";
 
 type AppStateContextProps = {
   draggedItem: DragItem | null;
   lists: List[];
   getTasksByListId(id: string): Task[];
   dispatch: Dispatch<Action>;
+};
+
+type AppStateProviderProps = {
+  children: React.ReactNode;
+  initialState: AppState;
 };
 
 const appData: AppState = {
@@ -37,20 +43,28 @@ const AppStateContext = createContext<AppStateContextProps>(
   {} as AppStateContextProps
 );
 
-export const AppStateProvider: FC = ({ children }) => {
-  const [state, dispatch] = useImmerReducer(appStateReducer, appData);
-  const { draggedItem, lists } = state;
-  const getTasksByListId = (id: string) => {
-    return lists.find((list) => list.id === id)?.tasks || [];
-  };
-  return (
-    <AppStateContext.Provider
-      value={{ draggedItem, lists, getTasksByListId, dispatch }}
-    >
-      {children}
-    </AppStateContext.Provider>
-  );
-};
+export const AppStateProvider = withInitialState<AppStateProviderProps>(
+  ({ children, initialState }) => {
+    const [state, dispatch] = useImmerReducer(appStateReducer, initialState);
+
+    useEffect(() => {
+      save(state);
+    }, [state]);
+
+    const { draggedItem, lists } = state;
+    const getTasksByListId = (id: string) => {
+      return lists.find((list) => list.id === id)?.tasks || [];
+    };
+
+    return (
+      <AppStateContext.Provider
+        value={{ draggedItem, lists, getTasksByListId, dispatch }}
+      >
+        {children}
+      </AppStateContext.Provider>
+    );
+  }
+);
 
 // Ths hook get the value from the AppStateContext using the useContext Hook.
 // Global context.
